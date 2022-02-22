a6c660b48d540228b22e1438c320d4c27f3858a0
Thomas Maier <l0922@schueler.ms-muc.de>
2022-02-22 10:40:27 +0100

    TOF Ausschaltverzögerung mit 2 Ausgänge

--- TestTimeDelay/main/examples/EXAMPLE_TOF/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TOF/main.cpp
@@ -20,7 +20,9 @@ static const char * const TAG = "EXAMPLE_TON";
 
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
+#define BUTTON_I2 GPIO_NUM_32        // Pin 32.
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
+#define GPIO_Q2 GPIO_NUM_23
 
 
 
@@ -38,26 +40,38 @@ extern "C" void app_main(void)
        functions.)
     */
     gpio_reset_pin(GPIO_Q1);
+    gpio_reset_pin(GPIO_Q2);
     gpio_reset_pin(BUTTON_I1);
+    gpio_reset_pin(BUTTON_I2);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_Q2, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
+    gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
+    gpio_set_level(GPIO_Q2, 0); //set to 0 at Reset.
+
 
 
     TOF TOF1;
     TOF1.PT = 1000;
 
+    TOF TOF2;
+        TOF2.PT = 3000;
+
     while (true) // Endlos-Schleife
     {
         // Eingang lesen, das not wird gebraucht weil die Eingaenge bei losgelassenem Taster auf 3.3V sind, und der Taster auf GND schaltet.
         bool I1 = not gpio_get_level(BUTTON_I1);
+        bool I2 = not gpio_get_level(BUTTON_I2);
 
         // den I1 an TOF2 uebergeben, und TOF2 aufrufen
         TOF1(I1);
+        TOF2(I2);
 
         // Ausgaenge setzen
         gpio_set_level(GPIO_Q1, TOF1.Q);
+        gpio_set_level(GPIO_Q2, TOF2.Q);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
